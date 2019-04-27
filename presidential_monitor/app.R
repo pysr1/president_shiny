930b364926aae34ae11a5c47da5d6af3446bd49e
Bryant Crocker <Crocker456@gmail.com>
2019-04-27 15:37:55 -0400

    update

--- presidential_monitor/app.R
+++ presidential_monitor/app.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(pageviews)
 #dates
 library(lubridate)
+library(billboarder)
 
 # define a function to get a subjects weekly pageviews
 
@@ -14,7 +15,7 @@ get_subjects = function(term){
     # select the only the date and views column
     
     pageviews = article_pageviews(project = "en.wikipedia", article = term
-                                  , start = as.Date('2018-12-01'), end = Sys.Date()
+                                  , start = as.Date('2018-09-01'), end = Sys.Date()
                                   , user_type = c("user"), platform = c("desktop", "mobile-web")) %>% select(date, views)
     #return a dataframe
     return(pageviews)
@@ -32,7 +33,7 @@ get_weekly_subjects = function(term){
         # calcualte total traffic
         summarize(Weekly_Traffic =sum(views))
     # create a column with the same name as the term
-    df[,term] = log10(df$Weekly_Traffic)
+    df[,term] = df$Weekly_Traffic
     #The last week will never be complete so don't want to include it since we are taking a sum
     n = length(df$Weekly_Traffic) - 1
     return(df %>% head(n) %>% select(-Weekly_Traffic))
@@ -44,13 +45,13 @@ get_daily_subjects = function(term){
     df = df %>% 
         # group by week
         group_by(date) %>%
+        rename(Date = date) %>% 
         # calcualte total traffic
         summarize(Weekly_Traffic =sum(views))
     # create a column with the same name as the term
-    df[,term] = log10(df$Weekly_Traffic)
+    df[,term] = df$Weekly_Traffic
     #The last week will never be complete so don't want to include it since we are taking a sum
-    n = length(df$Weekly_Traffic) - 1
-    return(df %>% head(n) %>% select(-Weekly_Traffic))
+    return(df  %>% select(-Weekly_Traffic))
 }
 
 
@@ -58,7 +59,7 @@ candidates = c(
     "Cory Booker",
     "Pete Buttigieg",
     "Julian_Castro",
-    "John Delaney",
+    #"John Delaney",
     "Tulsi Gabbard",
     "Kirsten Gillibrand",
     "Mike Gravel",
@@ -66,9 +67,10 @@ candidates = c(
     "John Hickenlooper",
     "Jay Inslee",
     "Amy Klobuchar",
-    "Wayne Messam",
+    #"Wayne Messam",
+    "Joe Biden",
     "Beto O'Rourke",
-    "Tim Ryan",
+    #"Tim Ryan",
     "Bernie Sanders",
     "Eric Swalwell",
     "Elizabeth Warren",
@@ -81,7 +83,7 @@ candidates = c(
 # the map function takes the list of subjects and call the get_weekly_subjects function on each element of the list
 # the reduce function uses left joins to create a subjects
 
-subjects = map(candidates, get_weekly_subjects) %>% reduce(left_join, by = "week")
+subjects = map(candidates, get_daily_subjects) %>% reduce(left_join, by = "Date")
 
 
 
@@ -92,8 +94,8 @@ header <- dashboardHeader(title = "Presidential Monitoring")
 #Sidebar content of the dashboard
 sidebar <- dashboardSidebar(
     sidebarMenu(
-        menuItem("All Candidates Time Series", tabName = "All Candidates Time Series", icon = icon("dashboard")),
-        menuItem("All Candidates Bar Series", tabName = "All Candidates Time Series", icon = icon("dashboard")),
+        menuItem("All Candidates Time Series", tabName = "plot_line", icon = icon("dashboard")),
+        menuItem("All Candidates Bar Chart", tabName = "plot_line2", icon = icon("dashboard")),
         menuItem("Visit my linkedin", icon = icon("send",lib='glyphicon'), 
                  href = "https://www.linkedin.com/in/bcrocke1/")
     )
@@ -104,19 +106,40 @@ sidebar <- dashboardSidebar(
 
 server <- function(input, output) {
     output$plot <- renderPlot({
-        subjects %>% gather(key, value, -week) %>% ggplot(aes(week, value))+
-        geom_point( size = 0.8)+
-        geom_smooth(method = "loess", se = T)+
-        #geom_line()+
-        labs( x = "Date", y= "Log10 Weekly Searchs")+
-        facet_wrap(~key)+#, scales = "free_y")+
-        theme_classic()
+            subjects %>% gather(key, value, -Date) %>% 
+            mutate(value = log10(value)) %>% 
+            ggplot(aes(Date, value))+
+            geom_smooth(method = "loess", se = T)+
+            geom_line(alpha = 0.3)+
+            labs( x = "Date", y= "Log10 Weekly Searchs")+
+            facet_wrap(~key)+#, scales = "free_y")+
+            theme_classic()
+        
+        
+    })
+    output$plot2 <- renderBillboarder({
+        agg = subjects %>% gather(key, value, -Date) %>% group_by(key) %>% summarise(value = mean(value, na.rm = T)) %>% 
+            arrange(desc(value)) %>% rename('Average Wikipedia Page Views' = value)
+        billboarder() %>% 
+            bb_barchart(data = agg, rotated = T) %>% 
+            bb_legend(show = FALSE) %>% 
+            bb_title("Average Daily Wikipedia Page Views")
+        
     })
 }
 
-body <- dashboardBody( 
-    plotOutput("plot", height = 700)
-)
+body <- dashboardBody(
+    tabItems(
+        tabItem(tabName = "plot_line",
+                plotOutput("plot", height = 700)),    
+        
+        
+        tabItem(tabName = "plot_line2",
+                billboarderOutput("plot2", height = 700))   
+    )  
+)  
+
+
 
 ui <- dashboardPage(
     header = header,
